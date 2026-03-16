dc62f58e038c1b2ae9e7b7f1546618f7039fb664
Andre Felipe <andrefelipe@vieiracred.com.br>
2026-03-16 09:16:27 -0300

    Accept email fallback for prata register login

--- app/Http/Controllers/Api/CriacaoController.php
+++ app/Http/Controllers/Api/CriacaoController.php
@@ -1083,7 +1083,8 @@ class CriacaoController extends Controller
     public function presenca_cadastro(Request $request)
     {
         try {
-            $login     = trim((string) $request->input('login', ''));
+            $loginInput = $request->input('login', $request->input('email', ''));
+            $login     = trim((string) $loginInput);
             $senha     = (string) $request->input('senha');
             $equipeIds = $this->normalizeApiEquipeIds($request->input('equipe_id', [1]));
 
